c82254feffdf7e409d6caa81c154bbd64ffa9bfd
David Chen <davidc3287@gmail.com>
2022-05-19 09:48:14 -0400

    add images

--- components/About.tsx
+++ components/About.tsx
@@ -2,6 +2,10 @@ export default function About() {
   return (
     <section className="section has-background-dark">
       <h1 className="title has-text-centered has-text-light">About Me</h1>
+      <div className="columns has-text-light has-text-centered">
+        <div className="column is-one-third">hi</div>
+        <div className="column is-two-thirds">hi</div>
+      </div>
     </section>
   );
 }
