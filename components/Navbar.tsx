bef762f98881b2c7674a0a76760a1d07b290b622
David Chen <davidc3287@gmail.com>
2022-05-19 09:15:09 -0400

    nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,15 +2,18 @@ import Image from "next/image";
 
 export function Navbar() {
   return (
-    <nav id="navbar-main" className="navbar is-fixed-top is-dark">
+    <nav id="navbar-main" className="navbar is-fixed-top is-dark is-size-5">
       <div className="navbar-brand">
-        {/* <h1 className="title has-text-light"> */}
         <Image src="/images/cube.png" width={50} height={50} alt="Home" />
-        {/* </h1> */}
       </div>
       <div className="navbar-menu">
         <div className="navbar-end">
-          <div className="navbar-item">hi</div>
+          <a className="navbar-item px-4 mx-1">Home</a>
+          <a className="navbar-item px-4 mx-1">About</a>
+          <a className="navbar-item px-4 mx-1">Projects</a>
+          <a className="navbar-item px-4 mx-1">Skills</a>
+          <a className="navbar-item px-4 mx-1">Books</a>
+          <a className="navbar-item px-4 mx-1">Resume</a>
         </div>
       </div>
     </nav>
