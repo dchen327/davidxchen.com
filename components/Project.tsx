b427aa3420a656e86afa5de87ca31fba9b3c565b
David Chen <davidc3287@gmail.com>
2022-05-20 13:53:41 -0400

    remove shadows

--- components/Project.tsx
+++ components/Project.tsx
@@ -98,8 +98,6 @@ function ProjectDesc(props: ProjectProps) {
         marginBottom: "5rem",
         border: "0.5px solid",
         borderRadius: "10px",
-        boxShadow:
-          "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px",
       }}
     >
       <h1
