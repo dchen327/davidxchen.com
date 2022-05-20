19b9e4bbce391ad34fc1c7d6ac75ae9bc8a6ef19
David Chen <davidc3287@gmail.com>
2022-05-20 08:53:47 -0400

    dark footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,8 @@
 export function Footer() {
   return (
     <section className="section p-0">
-      <footer className="footer py-4">
-        <div className="content has-text-centered">
-          <strong>davidxchen.com</strong> by David Chen. Built with Next.js and
-          Bulma.
-        </div>
+      <footer className="footer py-4 has-background-dark has-text-centered has-text-light">
+        <b>davidxchen.com</b> by David Chen. Built with Next.js and Bulma.
       </footer>
     </section>
   );
