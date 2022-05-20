ddfe613ea23f25ee0988bd433dc422113edcbf46
David Chen <davidc3287@gmail.com>
2022-05-20 13:29:59 -0400

    light skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,13 +2,11 @@ import Divider from "./Divider";
 
 export default function Skills() {
   return (
-    <section id="Skills" className="section has-background-dark">
-      <h1 className="title is-2 has-text-centered has-text-light">
-        Skills and Technologies
-      </h1>
+    <section id="Skills" className="section has-background-link-light">
+      <h1 className="title is-2 has-text-centered">Skills and Technologies</h1>
       <Divider />
       <div className="columns is-centered">
-        <div className="content has-text-light is-size-5">
+        <div className="content is-size-5">
           <ul>
             <li>
               <b>Programming Languages:</b> Fluent in Python; Familiar with
