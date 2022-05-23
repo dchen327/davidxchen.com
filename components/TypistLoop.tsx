2c0a5757989eef28d06e4387ec0441d7980a4419
David Chen <davidc3287@gmail.com>
2022-05-23 14:54:14 -0400

    loop component

---
+++ components/TypistLoop.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Typist from "react-typist";
+
+export default function TypistLoop({ words }: { words: string[] }) {
+  const [index, setIndex] = useState(0);
+
+  let word = words[index];
+
+  return (
+    <Typist
+      onTypingDone={() => {
+        setIndex((i) => (i + 1) % words.length);
+      }}
+    >
+      {word}
+      <Typist.Backspace count={word.length} delay={word.length * 100} />
+    </Typist>
+  );
+}
