f1f95e1c83886155e0cb96036953bc9bd2f77dff
David Chen <davidc3287@gmail.com>
2022-06-08 21:28:20 -0400

    images

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Divider from "./Divider";
 import Heading from "./Heading";
 
@@ -6,11 +7,36 @@ export default function Experience() {
     <section
       id="Experience"
       className="section has-text-light"
-      style={{ backgroundColor: "#25274D" }} 
+      style={{ backgroundColor: "#25274D" }}
     >
       <Heading text="Experience" />
       <Divider />
-      <div className="container" style={{ width: "80%" }}></div>
+      <div className="container has-background-white" style={{ width: "80%" }}>
+        <div className="columns has-text-centered is-gapless">
+          <div
+            className="column"
+            style={{ position: "relative", minHeight: "200px" }}
+          >
+            <Image
+              src="/images/hireflow.png"
+              layout="fill"
+              objectFit="contain"
+              alt="hireflow.png"
+            />
+          </div>
+          <div
+            className="column"
+            style={{ position: "relative", minHeight: "200px" }}
+          >
+            <Image
+              src="/images/slingshot.png"
+              layout="fill"
+              objectFit="contain"
+              alt="slingshot.png"
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
